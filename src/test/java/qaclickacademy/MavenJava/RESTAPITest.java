3f5f1d94dfcf7be8c07ec14270970455fd9055d9
adityathuse1989 <adityathuse1989@gmail.com>
2020-05-22 01:12:59 +0530

    first commit

---
+++ src/test/java/qaclickacademy/MavenJava/RESTAPITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void postJIra()
+	{
+		System.out.println("postJira");
+	}
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+}
